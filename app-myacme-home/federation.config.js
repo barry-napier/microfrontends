898b61c29bf32a75f91e356e17c1fabd1efc5aa2
Barry Napier <barry.a.napier@gmail.com>
2025-01-22 20:06:23 +0000

    Refactor Angular configuration for native federation: update project structure, enhance routing with HomeComponent, and streamline build options. Adjusted serve port and cleaned up federation config for better clarity.

--- app-myacme-home/federation.config.js
+++ app-myacme-home/federation.config.js
@@ -1,26 +1,28 @@
-const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+const {
+  withNativeFederation,
+  shareAll,
+} = require("@angular-architects/native-federation/config");
 
 module.exports = withNativeFederation({
-
-  name: 'app-myacme-home',
+  name: "app-myacme-home",
 
   exposes: {
-    './Component': './src/app/app.component.ts',
+    "./HomeComponent": "./src/app/home/home.component.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
 
   skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
+    "rxjs/ajax",
+    "rxjs/fetch",
+    "rxjs/testing",
+    "rxjs/webSocket",
     // Add further packages you don't need at runtime
-  ]
-
-  // Please read our FAQ about sharing libs:
-  // https://shorturl.at/jmzH0
-  
+  ],
 });
